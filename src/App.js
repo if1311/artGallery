3e48925b508f9accb18f17f43ac7c250c9b16b3e
Ioana Alexandra Filip <ioannaf.@Ioanas-MacBook-Pro.local>
2020-04-15 11:57:19 +0300

    app component empty + installed spring react library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./Components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+  return;
 }
 
 export default App;
