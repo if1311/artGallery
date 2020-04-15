46ae921186e7b4b2a7243d10a0bde9dc0461bb93
Alec Adelman <adelmanalec@gmail.com>
2020-04-15 17:20:00 +0300

    added carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Carousel from './components/Carousel';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <Carousel title ="Irises" author="Vincent Willem van Gogh" image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Mona_Lisa.jpg/1200px-Mona_Lisa.jpg"/>
+      <Homepage title ="Irises" author="Vincent Willem van Gogh" image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Mona_Lisa.jpg/1200px-Mona_Lisa.jpg"/>
     </div>
   );
 }
