ac82d409a1cd9b09419c015122fb4374bd327d1d
Ioana Alexandra Filip <ioannaf.@Ioanas-MacBook-Pro.local>
2020-05-13 18:00:40 +0300

    router for the logo

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Mgal from "./components/MGal";
 import "./App.css";
-import ArtistList from "./components/ArtistList"
+import ArtistList from "./components/ArtistList";
 import { Switch, Route } from "react-router-dom";
-import Dummy from "./components/Dummy";
+
 import Homepage from "./components/Homepage";
 
 function App() {
-	return (
-		<div>
-			<Switch>
-				<Route exact path="/" component={Homepage} />
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
 
-				<Route path="/gallery" component={Mgal} />
+        <Route path="/gallery" component={Mgal} />
 
-				<Route path="/artists" component={ArtistList} />
-
-				<Route path="/contact" component={Dummy} />
-			</Switch>
-
-		</div>
-	);
+        <Route path="/artists" component={ArtistList} />
+      </Switch>
+    </div>
+  );
 }
 export default App;
