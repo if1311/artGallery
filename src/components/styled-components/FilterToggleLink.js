c9fa95329e2d3b5268aac0b99c2f24fc9aab8d77
char0n9 <59369140+char0n9@users.noreply.github.com>
2020-05-14 10:58:00 +0300

    removed unused files, added margin to filterwrapper

--- src/components/styled-components/FilterToggleLink.js
+++ src/components/styled-components/FilterToggleLink.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const FilterToggleLink = styled.li`
-	/* position: relative; */
-	/* top: 20%; */
-	border: none;
-	cursor: pointer;
-	color: #353535;
-	font-size: 1.3em;
-	margin-left: 10%;
+  /* position: relative; */
+  /* top: 20%; */
+  border: none;
+  cursor: pointer;
+  color: #353535;
+  font-size: 1.3em;
+  margin-left: 10%;
+  /* margin-bottom: 2.5%; */
 `;
--- src/components/styled-components/FiltersWrapper.js
+++ src/components/styled-components/FiltersWrapper.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 export const FiltersWrapper = styled.ul`
-	width: 80%;
-	display: flex;
-	flex-flow: row nowrap;
-	background-color: inherit;
-	justify-content: space-around;
-	color: #284b63;
-	list-style-type: none;
+  width: 80%;
+  display: flex;
+  flex-flow: row nowrap;
+  background-color: inherit;
+  justify-content: space-around;
+  color: #284b63;
+  list-style-type: none;
+  margin-bottom: 2.5%;
 `;
