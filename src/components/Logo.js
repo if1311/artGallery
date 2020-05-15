8812c8ea3928f2deeb8477ae9e4a372ecb0fbb16
Iancu Andrei <29271791+iancuandrei@users.noreply.github.com>
2020-05-15 12:57:49 +0300

    changed url

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,25 +3,24 @@ import "./Logo.css";
 import { Link } from "react-router-dom";
 
 class Logo extends React.Component {
-  render() {
-    console.log(window.location.href);
-    return (
-      <div style={{ display: this.props.display ? "none" : "" }}>
-        <Link to="/">
-          <img
-            className="logo"
-            src={
-              this.props.white ||
-              window.location.href === "http://localhost:3000/"
-                ? require("../logos/logo_transparent_white.png")
-                : require("../logos/logo_transparent.png")
-            }
-            alt=""
-          />
-        </Link>
-      </div>
-    );
-  }
+	render() {
+		console.log(window.location.href);
+		return (
+			<div style={{ display: this.props.display ? "none" : "" }}>
+				<Link to="/">
+					<img
+						className="logo"
+						src={
+							this.props.white || window.location.href === "https://github.com/WildCodeSchool/bucharest-project2-art-gallery/"
+								? require("../logos/logo_transparent_white.png")
+								: require("../logos/logo_transparent.png")
+						}
+						alt=""
+					/>
+				</Link>
+			</div>
+		);
+	}
 }
 
 export default Logo;
